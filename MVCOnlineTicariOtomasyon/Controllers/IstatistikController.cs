bdd66ecbbbc2f85d3699b0aaa8a56e81de650924
asli akkaya <akkayaasli19@gmail.com>
2022-04-14 04:15:48 +0300

    partial view kullanımı -
    partial view kullanımı - partial view ile gruplandırma

--- MVCOnlineTicariOtomasyon/Controllers/IstatistikController.cs
+++ MVCOnlineTicariOtomasyon/Controllers/IstatistikController.cs
@@ -17,16 +17,16 @@ namespace MVCOnlineTicariOtomasyon.Controllers
             ViewBag.d1 = deger1;
             var deger2 = c.Uruns.Count().ToString();
             ViewBag.d2 = deger2;
-            var deger3= c.Personels.Count().ToString();
+            var deger3 = c.Personels.Count().ToString();
             ViewBag.d3 = deger3;
             var deger4 = c.Kategoris.Count().ToString();
             ViewBag.d4 = deger4;
 
 
 
-            var deger5 = c.Uruns.Sum(x=>x.Stok).ToString();
+            var deger5 = c.Uruns.Sum(x => x.Stok).ToString();
             ViewBag.d5 = deger5;
-            var deger6 = c.Uruns.Count(x=>x.Stok<20).ToString();
+            var deger6 = c.Uruns.Count(x => x.Stok < 20).ToString();
             ViewBag.d6 = deger6;
             //ürünler içinden markayı seç ve markayı tekrarsız olarak yaz Distinct metotu!!
             var deger7 = (from x in c.Uruns select x.Marka).Distinct().Count().ToString();
@@ -39,14 +39,14 @@ namespace MVCOnlineTicariOtomasyon.Controllers
             //min fiyatlı ürün adı:
             var deger9 = (from x in c.Uruns orderby x.SatisFiyat ascending select x.UrunAd).FirstOrDefault();
             ViewBag.d9 = deger9;
-            var deger10 = c.Uruns.Count(x=>x.UrunAd== "BUZDOLABI").ToString();
+            var deger10 = c.Uruns.Count(x => x.UrunAd == "BUZDOLABI").ToString();
             ViewBag.d10 = deger10;
             var deger11 = c.Uruns.Count(x => x.UrunAd == "Macbook").ToString();
-            ViewBag.d11= deger11;
+            ViewBag.d11 = deger11;
 
 
             //ürünleri markaya göre grupla ve ne çok hangi marka varsa onu getir.
-            var deger12 = c.Uruns.GroupBy(x=>x.Marka).OrderByDescending(z=>z.Count()).Select(y=>y.Key).
+            var deger12 = c.Uruns.GroupBy(x => x.Marka).OrderByDescending(z => z.Count()).Select(y => y.Key).
                 FirstOrDefault();
             ViewBag.d12 = deger12;
 
@@ -58,17 +58,39 @@ namespace MVCOnlineTicariOtomasyon.Controllers
             ViewBag.d13 = deger13;
 
 
-            var deger14 = c.SatisHarekets.Sum(x=>x.ToplamTutar).ToString();
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
             ViewBag.d14 = deger14;
 
             DateTime bugun = DateTime.Today;
-            var deger15 = c.SatisHarekets.Count(x=>x.Tarih==bugun).ToString();
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == bugun).ToString();
             ViewBag.d15 = deger15;
 
-            var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y=>y.ToplamTutar).ToString();
+            var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y => y.ToplamTutar).ToString();
             ViewBag.d16 = deger16;
 
             return View();
+        }
+        public ActionResult KolayTablolar()
+        {
+            var sorgu = from x in c.Carilers
+                        group x by x.CariSehir into g
+                        select new SinifGrup
+                        {
+                            Sehir = g.Key,
+                            Sayi = g.Count()
+                        };
+            return View(sorgu.ToList());
+        }
+        public PartialViewResult Partial1()
+        {
+            var sorgu2 = from x in c.Personels
+                         group x by x.Departmanid into g
+                         select new SinifGrup2
+                         {
+                             Departman = g.Key,
+                             Sayi = g.Count()
+                         };
+            return PartialView(sorgu2.ToList());
         }
     }
 }
